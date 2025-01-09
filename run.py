97e6969123340d2c3c15c1adbe66ad0328c6c174
Reuben Abrahams <reubenabrahams77@gmail.com>
2025-01-09 10:28:49 +0000

    feat: Add a function to validate user input for menu selections

--- run.py
+++ run.py
@@ -1,2 +1,21 @@
-import os
+def get_user_choice():
+    """
+    Display menu options and return user's choice.
+
+    Returns an integer of User's menu choice.
+    """
+    print("Income and Expense Tracker Menu:\n")
+    print("1. Add income or expense")
+    print("2. View transactions")
+    print("3. Check balance")
+    print("4. Exit")
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-4): "))
+            if 1 <= choice <= 4:
+                return choice
+            else:
+                print("Invalid choice. Please enter a number between 1 and 4.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
